8b7a1002dc6d03c73e037930ee7dd3015eafdced
Bhone Wai <damenaosan@gmail.com>
2025-08-11 22:03:14 +0700

    feat: register inventory routes in adminRoutes

--- server/src/routes/adminRoutes.ts
+++ server/src/routes/adminRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import productsRoute from "@routes/admin/products";
+import inventoryRoute from "@routes/admin/inventory";
 
 const router = Router();
 
 router.use('/products', productsRoute);
+router.use('/inventory', inventoryRoute);
 
 export default router;
