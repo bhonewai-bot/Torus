7f1ea0d8eec3ae8bb699e5522173e4c6a20a8ba6
Bhone Wai <damenaosan@gmail.com>
2025-08-11 21:53:02 +0700

    types: define ProductResponse interface for product API responses

--- server/src/types/ProductResponse.ts
+++ server/src/types/ProductResponse.ts
@@ -1,17 +1,3 @@
-/*
-export interface ProductResponse {
-    id: string;
-    name: string;
-    sku: string;
-    description?: string;
-    price: number;
-    categories: string[];
-    images: string[];
-    quantity: number;
-    createdAt: Date;
-    updatedAt: Date;
-}*/
-
 export interface ProductResponse {
     id: string;
     name: string;
