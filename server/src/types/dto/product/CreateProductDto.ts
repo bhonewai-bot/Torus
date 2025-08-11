235c73610a878014c9dc70821c0daa07630b521e
Bhone Wai <damenaosan@gmail.com>
2025-08-11 21:48:35 +0700

    refactor(inventory): remove comments

--- server/src/types/dto/product/CreateProductDto.ts
+++ server/src/types/dto/product/CreateProductDto.ts
@@ -1,14 +1,3 @@
-/*
-export interface CreateProductDto {
-    name: string;
-    sku: string;
-    description?: string;
-    price: number;
-    categories: string[];
-    images: string[];
-    quantity: number;
-}*/
-
 export interface CreateProductDto {
     name: string;
     sku: string;
