df881ca69e56192e7476e9025f5a9fdd39c858d9
Bhone Wai <damenaosan@gmail.com>
2025-08-12 21:51:20 +0700

    feat(admin): integrate user and order routes in adminRoutes

--- server/src/routes/adminRoutes.ts
+++ server/src/routes/adminRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import productsRoute from "@routes/admin/products";
 import inventoryRoute from "@routes/admin/inventory";
+import userRoute from "@routes/admin/users";
+import orderRoute from "@routes/admin/orderes"
 
 const router = Router();
 
 router.use('/products', productsRoute);
 router.use('/inventory', inventoryRoute);
+router.use('/users', userRoute);
+router.use('/orders', orderRoute);
 
 export default router;
