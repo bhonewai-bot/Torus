8a33bb29b39050eabe7d24af7adeb3d72cea42f7
Bhone Wai <damenaosan@gmail.com>
2025-08-21 20:30:41 +0700

    fix(next): Add new image domains to next.config.ts
    This commit adds the following image domains to the next.config.ts file:
    
    * m.media-amazon.com
    
    * media-cdn.bnn.in.th
    
    * image.uniqlo.com

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'm.media-amazon.com',
+      'media-cdn.bnn.in.th',
+      'image.uniqlo.com',
+      // Add other domains for your product images
+    ],
+  },
 };
 
 export default nextConfig;
